72489bde72cf4ae99d3a510c81085f20145c0084
PreetiPingale <145134565+PreetiPingale@users.noreply.github.com>
2025-01-29 09:41:34 +0000

    hashing

---
+++ hashing_map/hash.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+ int hashh[10000000]={0};
+void hashing(int arr[],int n)
+{
+   
+    for(int i=0;i<n;i++)
+    {
+        hashh[arr[i]]++;
+    }
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int i;
+        cin>>i;
+        cout<<hashh[i]<<" ";
+    }
+}
+int main()
+{
+   int n;
+   cin>>n;
+   int arr[n];
+   for(int i=0;i<n;i++)
+   {
+    cin>>arr[i];
+   }
+   hashing(arr,n);
+}
